6565e300309a89f859fd62d2b626ca84675d9ae8
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-03-06 21:07:50 +0200

    0.3.2 - fixed wine `LANG` codes

--- src/config/game/wine/wine_lang.rs
+++ src/config/game/wine/wine_lang.rs
@@ -38,16 +38,16 @@ impl WineLang {
         HashMap::from([("LANG", match self {
             Self::System => return HashMap::new(),
 
-            Self::English    => "en_US.UTF8",
-            Self::Russian    => "ru_RU.UTF8",
-            Self::German     => "de_DE.UTF8",
-            Self::Portuguese => "pt_PT.UTF8",
-            Self::Polish     => "pl_PL.UTF8",
-            Self::French     => "fr_FR.UTF8",
-            Self::Spanish    => "es_ES.UTF8",
-            Self::Chinese    => "zh_CN.UTF8",
-            Self::Japanese   => "ja_JP.UTF8",
-            Self::Korean     => "ko_KR.UTF8"
+            Self::English    => "en_US.UTF-8",
+            Self::Russian    => "ru_RU.UTF-8",
+            Self::German     => "de_DE.UTF-8",
+            Self::Portuguese => "pt_PT.UTF-8",
+            Self::Polish     => "pl_PL.UTF-8",
+            Self::French     => "fr_FR.UTF-8",
+            Self::Spanish    => "es_ES.UTF-8",
+            Self::Chinese    => "zh_CN.UTF-8",
+            Self::Japanese   => "ja_JP.UTF-8",
+            Self::Korean     => "ko_KR.UTF-8"
         })])
     }
 }
