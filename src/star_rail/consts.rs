3b1bae948dae4051ca1b3f106278d3eb8c23eca9
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-04-28 16:20:04 +0200

    1.1.3 - added `LAUNCHER_FOLDER` variable support

--- src/star_rail/consts.rs
+++ src/star_rail/consts.rs
@@ -2,9 +2,13 @@ use std::path::PathBuf;
 
 /// Get default launcher dir path
 /// 
-/// `$HOME/.local/share/honkers-railway-launcher`
+/// If `LAUNCHER_FOLDER` variable is set, then its value will be returned. Otherwise return `$HOME/.local/share/honkers-railway-launcher`
 #[inline]
 pub fn launcher_dir() -> anyhow::Result<PathBuf> {
+    if let Ok(folder) = std::env::var("LAUNCHER_FOLDER") {
+        return Ok(folder.into());
+    }
+
     Ok(std::env::var("XDG_DATA_HOME")
         .or_else(|_| std::env::var("HOME").map(|home| home + "/.local/share"))
         .map(|home| PathBuf::from(home).join("honkers-railway-launcher"))?)
