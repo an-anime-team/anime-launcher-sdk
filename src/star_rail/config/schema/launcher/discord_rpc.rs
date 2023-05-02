4144faf0cd1719052c09ce531a6a0a5e2043ca15
Marie <marie@kaifa.ch>
2023-05-02 19:01:40 +0200

    Update discord_rpc.rs

--- src/star_rail/config/schema/launcher/discord_rpc.rs
+++ src/star_rail/config/schema/launcher/discord_rpc.rs
@@ -25,17 +25,15 @@ impl From<DiscordRpc> for DiscordRpcParams {
     }
 }
 
-// TODO: add honkers-specific discord rpc
-
 impl Default for DiscordRpc {
     #[inline]
     fn default() -> Self {
         Self {
-            app_id: 901534333360304168,
+            app_id: 1102997693040701481,
             enabled: false,
 
-            title: String::from("Researching the world"),
-            subtitle: String::from("of Teyvat"),
+            title: String::from("Trailblazing"),
+            subtitle: String::from("The Galaxy"),
             icon: String::from("launcher")
         }
     }
