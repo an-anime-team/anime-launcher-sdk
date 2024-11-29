97c8bb450d4d3090f2f511b00f3c37a21dcc5bc4
Nikita Podvirnyi <krypt0nn@vk.com>
2024-11-29 13:40:32 +0200

    refactor: fix upstream bug caused by !16

--- src/games/zzz/consts.rs
+++ src/games/zzz/consts.rs
@@ -13,33 +13,73 @@ lazy_static::lazy_static! {
 }
 
 /// Get default launcher dir path
-/// 
+///
 /// If `LAUNCHER_FOLDER` variable is set, then its value will be returned. Otherwise return `$HOME/.local/share/sleepy-launcher`
 pub fn launcher_dir() -> anyhow::Result<PathBuf> {
     if let Ok(folder) = std::env::var("LAUNCHER_FOLDER") {
         return Ok(folder.into());
     }
 
-    Ok(std::env::var("XDG_DATA_HOME")
-        .or_else(|_| std::env::var("HOME").map(|home| home + "/.local/share"))
-        .map(|home| PathBuf::from(home).join(FOLDER_NAME).canonicalize())??)
+    let path = std::env::var("XDG_DATA_HOME")
+        .map(|data| format!("{data}/{FOLDER_NAME}"))
+        .or_else(|_| {
+            std::env::var("HOME")
+                .map(|home| {
+                    format!("{home}/.local/share/{FOLDER_NAME}")
+                })
+        })
+        .or_else(|_| {
+            std::env::var("USER")
+                .or_else(|_| std::env::var("USERNAME"))
+                .map(|username| {
+                    format!("/home/{username}/.local/share/{FOLDER_NAME}")
+                })
+        })
+        .map(PathBuf::from)
+        .or_else(|_| {
+            std::env::current_dir()
+                .map(|current| current.join("data"))
+        })
+        .map_err(|err| anyhow::anyhow!("Failed to find launcher folder: {err}"))?;
+
+    path.canonicalize().or(Ok(path))
 }
 
 /// Get launcher's cache dir path
-/// 
+///
 /// If `CACHE_FOLDER` variable is set, then its value will be returned. Otherwise return `$HOME/.cache/sleepy-launcher`
 pub fn cache_dir() -> anyhow::Result<PathBuf> {
     if let Ok(folder) = std::env::var("CACHE_FOLDER") {
         return Ok(folder.into());
     }
 
-    Ok(std::env::var("XDG_CACHE_HOME")
-        .or_else(|_| std::env::var("HOME").map(|home| home + "/.cache"))
-        .map(|home| PathBuf::from(home).join(FOLDER_NAME).canonicalize())??)
+    let path = std::env::var("XDG_CACHE_HOME")
+        .map(|cache| format!("{cache}/{FOLDER_NAME}"))
+        .or_else(|_| {
+            std::env::var("HOME")
+                .map(|home| {
+                    format!("{home}/.cache/{FOLDER_NAME}")
+                })
+        })
+        .or_else(|_| {
+            std::env::var("USER")
+                .or_else(|_| std::env::var("USERNAME"))
+                .map(|username| {
+                    format!("/home/{username}/.cache/{FOLDER_NAME}")
+                })
+        })
+        .map(PathBuf::from)
+        .or_else(|_| {
+            std::env::current_dir()
+                .map(|current| current.join("cache"))
+        })
+        .map_err(|err| anyhow::anyhow!("Failed to find cache folder: {err}"))?;
+
+    path.canonicalize().or(Ok(path))
 }
 
 /// Get config file path
-/// 
+///
 /// Default is `$HOME/.local/share/sleepy-launcher/config.json`
 pub fn config_file() -> anyhow::Result<PathBuf> {
     launcher_dir().map(|dir| dir.join("config.json"))
